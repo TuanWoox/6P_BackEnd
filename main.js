76c334d3d7ae5b1535c98d9795d9acdc2a256fa4
canhhandsome <cbd1708@gmail.com>
2025-04-16 09:21:04 +0700

    Initial backend setup with Express, MongoDB, and basic config

--- main.js
+++ main.js
@@ -2,19 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors')
 const mongoose = require('mongoose');
+const connectDB = require("./config/db");
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.json()); // to parse JSON body
-app.use(express.urlencoded({ extended: true })); // to parse URL-encoded body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log('✅ Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-})
-.catch(err => console.error('Failed to connect MongoDB:', err));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+connectDB(); 
