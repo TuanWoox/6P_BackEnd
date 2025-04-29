3256d4edb7e771db8c7a6a52cf93f24283dc59ed
MrHH-k22 <nguyenmaihuyhoang156@gmail.com>
2025-04-29 16:00:00 +0700

    add feature createNewLoan

--- DAO/LoanTypeInterestRatesDAO.js
+++ DAO/LoanTypeInterestRatesDAO.js
@@ -10,6 +10,20 @@ class LoanTypeInterestRatesDAO {
       throw err;
     }
   }
+
+  async getLoanTypeInterestRatesByloanTypeAndloanTerm(loanType, loanTerm) {
+    try {
+      // Tìm kiếm theo loanType và termMonths (loanTerm)
+      const interestRate = await LoanTypeInterestRates.findOne({
+        loanType: loanType,
+        termMonths: Number(loanTerm),
+      });
+      console.log(interestRate);
+      return interestRate;
+    } catch (err) {
+      throw err;
+    }
+  }
 }
 
 module.exports = new LoanTypeInterestRatesDAO();
--- routes/loanAccount.js
+++ routes/loanAccount.js
@@ -10,8 +10,25 @@ router
     loanAccountController.getAllLoanAccounts
   );
 
+router.route("/getAllLoanTypes").get(loanAccountController.getAllLoanTypes);
+
 router
   .route("/details/:loanAccountId")
   .get(authMiddleware.authenticateToken, loanAccountController.getLoanAccount);
+
+router
+  .route("/getAllLoanInterestRates")
+  .get(loanAccountController.getAllLoanInterestRates);
+
+router
+  .route("/findLoanInterestRates")
+  .post(loanAccountController.findLoanInterestRates);
+
+router
+  .route("/createNewLoan")
+  .post(
+    authMiddleware.authenticateToken,
+    loanAccountController.createLoanAccount
+  );
 
 module.exports = router;
